c79219199017832caa373f8fdccc5af05c7f929a
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2023-04-27 17:24:50 +0300

    feat: implement auth page

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from '@/types'
+
+export const siteConfig: SiteConfig = {
+  name: 'Front-End Friend',
+  description:
+    'An open source application built using the new router, server components and everything new in Next.js 13.',
+  url: '',
+  links: {
+    twitter: 'https://twitter.com/MrBarracuda28',
+    github: 'https://github.com/MrBarracuda',
+  },
+}
